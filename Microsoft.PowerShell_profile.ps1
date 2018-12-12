bcaafbd9955d12b9c77e74a660ef0df3319301ec
Denny Gebel <denny.gebel@dilax.com>
2018-12-12 17:12:11 +0100

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Agnoster
+Import-Module PSConsoleTheme
+Set-ConsoleTheme 'Solarized Dark'
+
+function ll { dir -Force }
