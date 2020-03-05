c579d3be5a6888dfe4d4edaee402717c5e6c58bd
Denny <denny.gebel@foerch.de>
2020-03-05 10:23:03 +0100

    update profiles

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -4,16 +4,31 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-# Import-Module oh-my-posh
-# Import-Module posh-git
-# Import-Module PSConsoleTheme
+# set up persistent history
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadlineKeyHandler -Key ctrl+s -Function ClearScreen
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+
+
+Import-Module PSFzf -ArgumentList 'Ctrl+t','Ctrl+r'
+Import-Module oh-my-posh
+Import-Module posh-git
+Import-Module PSConsoleTheme
 
 Set-ConsoleTheme 'Solarized Dark'
 Set-Theme Agnoster
+
+
 
 function .. { Set-Location .. }
 function ... { Set-Location ..\.. }
 function ll { Get-ChildItem -Force }
 function tailf { Get-Content -Wait }
 function ws { Set-Location $Home\Documents\workspace }
-
