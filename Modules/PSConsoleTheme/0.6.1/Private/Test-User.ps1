0981a599fd0cd7fd7d7ae7f42d0875c7849c16c3
Denny <denny.gebel@foerch.de>
2020-11-12 19:02:28 +0100

    removed modules from repo

--- Modules/PSConsoleTheme/0.6.1/Private/Test-User.ps1
+++
@@ -1,9 +0,0 @@
-function Test-User {
-    param(
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-        [string] $Theme
-    )
-    Process {
-        $true
-    }
-}
--- Modules/oh-my-posh/2.0.399/Build/setup_test_env.ps1
+++
@@ -1,3 +0,0 @@
-if ($null -eq (Get-Module -ListAvailable pester)) {
-    Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
-}
