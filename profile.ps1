cc19d20fc9178b2d8b816e6dacf5beac1574ce1f
Denny <denny.gebel@foerch.de>
2021-02-09 09:37:16 +0100

    updated profile

--- profile.ps1
+++ profile.ps1
@@ -1,37 +1,37 @@
 # # Chocolatey profile
 # $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 # if (Test-Path($ChocolateyProfile)) {
-  # Import-Module "$ChocolateyProfile"
+# Import-Module "$ChocolateyProfile"
 # }
 
 # set up persistent history
 $HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
-Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
-if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | Out-Null
+if (Test-Path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
 
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -PredictionSource History
 
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
 Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
 Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
 Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
 Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
 Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-Set-PSReadlineKeyHandler -Key ctrl+s -Function ClearScreen
+Set-PSReadLineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadLineKeyHandler -Key ctrl+s -Function ClearScreen
 
-Remove-PSReadlineKeyHandler 'Ctrl+r'
+Remove-PSReadLineKeyHandler 'Ctrl+r'
 
 
-Import-Module PSFzf -ArgumentList 'Ctrl+t','Ctrl+r'
+Import-Module PSFzf -ArgumentList 'Ctrl+t', 'Ctrl+r'
 Import-Module oh-my-posh
 Import-Module posh-git
-Import-Module PSConsoleTheme
+# Import-Module PSConsoleTheme
 
-Set-ConsoleTheme 'Solarized Dark'
+# Set-ConsoleTheme 'Solarized Dark'
 Set-Theme Agnoster
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
@@ -41,5 +41,5 @@ function .. { Set-Location .. }
 function ... { Set-Location ..\.. }
 function ll { Get-ChildItem -Force }
 function tailf { Get-Content -Wait }
-function ws { Set-Location $Home\Documents\workspace }
+function ws { Set-Location c:\workspace }
 function grep { $input | Out-String -Stream | Select-String $args }
