646c1e66feba33310259da4c7b6875b99a4f9e36
Denny <denny.gebel@foerch.de>
2021-06-21 09:08:19 +0200

    updated profiles

---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,52 @@
+﻿# # Chocolatey profile
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#   Import-Module "$ChocolateyProfile"
+# }
+
+# helper
+function Import-Module-With-Measure {
+    param ($ModuleName)
+    $import = Measure-Command {
+        Import-Module $ModuleName
+    }
+    Write-Host "$ModuleName import $($import.TotalMilliseconds) ms"
+}
+
+Import-Module-With-Measure PSFzf -ArgumentList 'Ctrl+t', 'Ctrl+r'
+Import-Module-With-Measure oh-my-posh
+Import-Module-With-Measure posh-git
+
+# set up persistent history
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -PredictionSource History
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
+Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
+Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadlineKeyHandler -Key ctrl+s -Function ClearScreen
+
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+
+Set-PoshPrompt -Theme "~\OneDrive - Theo Förch GmbH & Co. KG\Dokumente\WindowsPowerShell\AgnosterWithGit.json"
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+function .. { Set-Location .. }
+function ... { Set-Location ..\.. }
+function ll { Get-ChildItem -Force }
+function tailf { Get-Content -Wait }
+function ws { Set-Location c:\workspace }
+function grep { $input | Out-String -Stream | Select-String $args }
